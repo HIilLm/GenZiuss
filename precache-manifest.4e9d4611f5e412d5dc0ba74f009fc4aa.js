370ca550ef421171275724a73641976fbfd4a221
HIilLm <97495105+HIilLm@users.noreply.github.com>
2022-12-16 22:14:00 +0700

    web

---
+++ precache-manifest.4e9d4611f5e412d5dc0ba74f009fc4aa.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89be038284f166d78096",
+    "url": "/css/app.25fd58e3.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicosan.ico"
+  },
+  {
+    "revision": "3fb4d6e148c026a1f59ab701a1adbd2e",
+    "url": "/img/bg rev.jpg"
+  },
+  {
+    "revision": "22b08b8ca43348fd2f679d545b93ee85",
+    "url": "/img/g0.webp"
+  },
+  {
+    "revision": "27b5ebba73a5efe9a92005a87224bb7d",
+    "url": "/img/pages/g-02.webp"
+  },
+  {
+    "revision": "9bbc93e57b6ac15aa74dbece6057aaa3",
+    "url": "/img/pages/g-03.webp"
+  },
+  {
+    "revision": "cded37a792977a879986e97182e8da9c",
+    "url": "/img/pages/g-04.webp"
+  },
+  {
+    "revision": "34737b658642d832dcdf422cbc5d2bf6",
+    "url": "/img/pages/g-05.webp"
+  },
+  {
+    "revision": "44a7e4a5db3cb8eb08542c5fc0f13665",
+    "url": "/img/pages/g-06.webp"
+  },
+  {
+    "revision": "fb6f5bc9dafb6e9f2dcc3895397331dd",
+    "url": "/img/pages/g-07.webp"
+  },
+  {
+    "revision": "52ba0420d35b16c78c5f3f6b0c622623",
+    "url": "/img/pages/g-08.webp"
+  },
+  {
+    "revision": "c38c13d9851bcb535f18326f55f003b4",
+    "url": "/img/pages/g-09.webp"
+  },
+  {
+    "revision": "bddecf324d66bdfdd1dcad0d81e474ed",
+    "url": "/img/pages/g-10.webp"
+  },
+  {
+    "revision": "9e8647010bd38a993f3b91687b25f44a",
+    "url": "/img/pages/g-11.webp"
+  },
+  {
+    "revision": "ec43c68d5900b7bc23c22ef4a475d736",
+    "url": "/img/pages/g-12.webp"
+  },
+  {
+    "revision": "43e3c72639f4767906bada873b46f10d",
+    "url": "/img/pages/g-13.webp"
+  },
+  {
+    "revision": "c4dfbb7c8d71ade312ed87b1bafa3aeb",
+    "url": "/img/pages/g-14.webp"
+  },
+  {
+    "revision": "e0a059cb84f00a9c72512da85f866b9d",
+    "url": "/img/pages/g-15.webp"
+  },
+  {
+    "revision": "552310bc02bd1034fa1083d0e73085f8",
+    "url": "/img/pages/gs-01.webp"
+  },
+  {
+    "revision": "b9620b886d29e1d8f45ed111d2445356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89be038284f166d78096",
+    "url": "/js/app.0d16cbde.js"
+  },
+  {
+    "revision": "1f7d24c889c19d038a42",
+    "url": "/js/chunk-vendors.c8aac5ce.js"
+  },
+  {
+    "revision": "4e0ff27224983f096937849b6496e7db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
